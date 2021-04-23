0530cf27cd0188d14e276fdc8f517c13e058d206
Ivan Ivanov <vankodev@gmail.com>
2021-04-23 10:24:24 +0300

    Create the Model logic

--- js/script.js
+++ js/script.js
@@ -31,23 +31,55 @@ class Model {
     ];
   }
 
-  // Add sentence
+  // Add sentence by its indexes
+  addSentence(text, p, s) {
+    this.revision[p].splice(s, 0, text);
+  }
 
   // Delete sentence
+  deleteSentence(p, s) {
+    this.revision[p].splice(s, 1);
+  }
 
   // Split paragraph in two paragraphs
+  splitParagraph(p, s) {
+    let part = this.revision[p].splice(s);
+    this.revision.splice(p + 1, 0, part);
+  }
 
   // Combine paragraph with next paragraph
+  combineParagraphs(p) {
+    let combined = this.revision[p].concat(this.revision[p + 1]);
+    this.revision.splice(p, p + 2, combined);
+  }
 
   // Add sentence variant
+  addVariant(text, p, s) {
+    this.revision[p][s].push(text);
+  }
 
   // Delete sentence variant
+  deleteVariant(p, s, v) {
+    this.revision[p][s].splice(v, 1);
+  }
 
-  // Chose best sentence variant
+  // Choose best sentence variant
+  chooseBest(p, s, v) {
+    let best = this.revision[p][s].splice(v, 1);
+    this.revision[p][s].unshift(...best);
+  }
 
-  // Move Sentence
+  // Move sentence by start and end indexes
+  moveSentence(p, s, p2, s2) {
+    let sentence = this.revision[p].splice(s, 1);
+    this.revision[p2].splice(s2, 0, ...sentence);
+  }
 
-  // Move Paragraph
+  // Move paragraph by start and end indexes
+  moveParagraph(p, p2) {
+    let paragraph = this.revision.splice(p, 1);
+    this.revision.splice(p2, 0, ...paragraph);
+  }
 }
 
 class View {
